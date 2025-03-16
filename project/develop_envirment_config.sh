117f2facade3f636d8de9340bc41ba82778fadf1
时空自由民 <ufo281@outlook.com>
2025-03-16 20:37:30 +0800

    [V1.1.0][sdk][add]{1.add pico example && pico sdk}

---
+++ project/develop_envirment_config.sh
@@ -0,0 +1,7 @@
+sudo apt update;
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential;
+
+# ubuntu should install
+sudo apt install g++ libstdc++-arm-none-eabi-newlib;
+
+export PICO_SDK_PATH=/home/wls/git/Raspberry_Pi_Pico_Learn_Note/project/pico-sdk;   
