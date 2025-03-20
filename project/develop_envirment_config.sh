4b8cf1bab4c03e407be1278714f1462318a465c5
时空自由民 <ufo281@outlook.com>
2025-03-19 19:40:42 -0700

    [V1.1.4][配置脚本][修改]{1.加入SDK_PATH配置 }

--- project/develop_envirment_config.sh
+++ project/develop_envirment_config.sh
@@ -4,4 +4,5 @@ sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
 # ubuntu should install
 sudo apt install g++ libstdc++-arm-none-eabi-newlib;
 
-export PICO_SDK_PATH=/home/wls/git/Raspberry_Pi_Pico_Learn_Note/project/pico-sdk;   
+# You need to manually configure the path to your real SDK
+export PICO_SDK_PATH=/home/wls/git/Raspberry_Pi_Pico_Learn_Note/project/pico-sdk;
